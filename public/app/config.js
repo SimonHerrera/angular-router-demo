8731fe0e250756e61aec90dba40fab086f8b8ca3
Simon Herrera <simonherrera.tech@gmail.com>
2016-06-02 08:59:43 -0500

    Add Notes to file

--- public/app/config.js
+++ public/app/config.js
@@ -7,14 +7,17 @@ angular.module("app")
       .when('/todo', { //added later
         templateUrl: 'app/partials/todo.html',
         controller: 'TodoCtrl'
+        // controllerAs: "dirList.TodoCtrl" (if converting from Scope)
       })
       .when('/hello', {
         templateUrl: 'app/partials/hello.html',
         controller: 'HelloCtrl',
+        // controllerAs: "dirList.HelloCtrl" (if converting from Scope)
       })
       .when('/hello/:name', {
         templateUrl: 'app/partials/hello-person.html',
         controller: 'HelloPersonCtrl',
+        // controllerAs: "dirList.HelloPersonCtrl" (if converting from Scope)
       })
       .otherwise('/')
   })
